cd64e03ffc130f9b9fc3018dfcbf0dc5e5488984
anggerpradana <anggermuliapradana@gmail.com>
2022-02-16 15:03:06 +0700

    fix: deprecated warning php 8.0

--- src/Traits/InvertValidationTrait.php
+++ src/Traits/InvertValidationTrait.php
@@ -21,7 +21,7 @@ trait InvertValidationTrait
      *
      * @return bool
      */
-    protected function validate_invert_required($field, array $input, array $params = [], $value)
+    protected function validate_invert_required($field, array $input, array $params, $value)
     {
         return !$this->validate_required($field, $input, $params, $value);
     }
@@ -36,9 +36,9 @@ trait InvertValidationTrait
      *
      * @return bool
      */
-    protected function validate_invert_contains($field, array $input, array $params = [], $value)
+    protected function validate_invert_contains($field, array $input, array $params, $value)
     {
-        return !$this->validate_contains($field, $input, $params, $value);
+        return !$this->validate_contains($field, $input, $params);
     }
 
     /**
@@ -51,9 +51,9 @@ trait InvertValidationTrait
      *
      * @return bool
      */
-    protected function validate_invert_contain_list($field, array $input, array $params = [], $value)
+    protected function validate_invert_contain_list($field, array $input, array $params, $value)
     {
-        return !$this->validate_contains($field, $input, $params, $value);
+        return !$this->validate_contains_list($field, $input, $params);
     }
 
     /**
@@ -66,9 +66,9 @@ trait InvertValidationTrait
      *
      * @return bool
      */
-    protected function validate_invert_doesnt_contain_list($field, array $input, array $params = [], $value)
+    protected function validate_invert_doesnt_contain_list($field, array $input, array $params, $value)
     {
-        return !$this->validate_doesnt_contain_list($field, $input, $params, $value);
+        return !$this->validate_doesnt_contain_list($field, $input, $params);
     }
 
     /**
@@ -81,7 +81,7 @@ trait InvertValidationTrait
      *
      * @return bool
      */
-    protected function validate_invert_boolean($field, array $input, array $params = [], $value)
+    protected function validate_invert_boolean($field, array $input, array $params, $value)
     {
         return !$this->validate_boolean($field, $input, $params, $value);
     }
@@ -96,7 +96,7 @@ trait InvertValidationTrait
      *
      * @return bool
      */
-    protected function validate_invert_valid_email($field, array $input, array $params = [], $value)
+    protected function validate_invert_valid_email($field, array $input, array $params, $value)
     {
         return !$this->validate_valid_email($field, $input, $params, $value);
     }
@@ -111,7 +111,7 @@ trait InvertValidationTrait
      *
      * @return bool
      */
-    protected function validate_invert_max_len($field, array $input, array $params = [], $value)
+    protected function validate_invert_max_len($field, array $input, array $params, $value)
     {
         return !$this->validate_max_len($field, $input, $params, $value);
     }
@@ -126,7 +126,7 @@ trait InvertValidationTrait
      *
      * @return bool
      */
-    protected function validate_invert_min_len($field, array $input, array $params = [], $value)
+    protected function validate_invert_min_len($field, array $input, array $params, $value)
     {
         return !$this->validate_min_len($field, $input, $params, $value);
     }
@@ -141,7 +141,7 @@ trait InvertValidationTrait
      *
      * @return bool
      */
-    protected function validate_invert_exact_len($field, array $input, array $params = [], $value)
+    protected function validate_invert_exact_len($field, array $input, array $params, $value)
     {
         return !$this->validate_exact_len($field, $input, $params, $value);
     }
@@ -156,7 +156,7 @@ trait InvertValidationTrait
      *
      * @return bool
      */
-    protected function validate_invert_between_len($field, array $input, array $params = [], $value)
+    protected function validate_invert_between_len($field, array $input, array $params, $value)
     {
         return !$this->validate_between_len($field, $input, $params, $value);
     }
@@ -171,7 +171,7 @@ trait InvertValidationTrait
      *
      * @return bool
      */
-    protected function validate_invert_alpha($field, array $input, array $params = [], $value)
+    protected function validate_invert_alpha($field, array $input, array $params, $value)
     {
         return !$this->validate_alpha($field, $input, $params, $value);
     }
@@ -186,7 +186,7 @@ trait InvertValidationTrait
      *
      * @return bool
      */
-    protected function validate_invert_alpha_numeric($field, array $input, array $params = [], $value)
+    protected function validate_invert_alpha_numeric($field, array $input, array $params, $value)
     {
         return !$this->validate_alpha_numeric($field, $input, $params, $value);
     }
@@ -201,7 +201,7 @@ trait InvertValidationTrait
      *
      * @return bool
      */
-    protected function validate_invert_alpha_dash($field, array $input, array $params = [], $value)
+    protected function validate_invert_alpha_dash($field, array $input, array $params, $value)
     {
         return !$this->validate_alpha_dash($field, $input, $params, $value);
     }
@@ -216,7 +216,7 @@ trait InvertValidationTrait
      *
      * @return bool
      */
-    protected function validate_invert_alpha_numeric_dash($field, array $input, array $params = [], $value)
+    protected function validate_invert_alpha_numeric_dash($field, array $input, array $params, $value)
     {
         return !$this->validate_alpha_numeric_dash($field, $input, $params, $value);
     }
@@ -231,7 +231,7 @@ trait InvertValidationTrait
      *
      * @return bool
      */
-    protected function validate_invert_alpha_numeric_space($field, array $input, array $params = [], $value)
+    protected function validate_invert_alpha_numeric_space($field, array $input, array $params, $value)
     {
         return !$this->validate_alpha_numeric_space($field, $input, $params, $value);
     }
@@ -246,7 +246,7 @@ trait InvertValidationTrait
      *
      * @return bool
      */
-    protected function validate_invert_alpha_space($field, array $input, array $params = [], $value)
+    protected function validate_invert_alpha_space($field, array $input, array $params, $value)
     {
         return !$this->validate_alpha_space($field, $input, $params, $value);
     }
@@ -261,7 +261,7 @@ trait InvertValidationTrait
      *
      * @return bool
      */
-    protected function validate_invert_numeric($field, array $input, array $params = [], $value)
+    protected function validate_invert_numeric($field, array $input, array $params, $value)
     {
         return !$this->validate_numeric($field, $input, $params, $value);
     }
@@ -276,7 +276,7 @@ trait InvertValidationTrait
      *
      * @return bool
      */
-    protected function validate_invert_integer($field, array $input, array $params = [], $value)
+    protected function validate_invert_integer($field, array $input, array $params, $value)
     {
         return !$this->validate_integer($field, $input, $params, $value);
     }
@@ -291,7 +291,7 @@ trait InvertValidationTrait
      *
      * @return bool
      */
-    protected function validate_invert_float($field, array $input, array $params = [], $value)
+    protected function validate_invert_float($field, array $input, array $params, $value)
     {
         return !$this->validate_float($field, $input, $params, $value);
     }
@@ -306,7 +306,7 @@ trait InvertValidationTrait
      *
      * @return bool
      */
-    protected function validate_invert_valid_url($field, array $input, array $params = [], $value)
+    protected function validate_invert_valid_url($field, array $input, array $params, $value)
     {
         return !$this->validate_valid_url($field, $input, $params, $value);
     }
@@ -321,7 +321,7 @@ trait InvertValidationTrait
      *
      * @return bool
      */
-    protected function validate_invert_url_exists($field, array $input, array $params = [], $value)
+    protected function validate_invert_url_exists($field, array $input, array $params, $value)
     {
         return !$this->validate_url_exists($field, $input, $params, $value);
     }
@@ -336,7 +336,7 @@ trait InvertValidationTrait
      *
      * @return bool
      */
-    protected function validate_invert_valid_ip($field, array $input, array $params = [], $value)
+    protected function validate_invert_valid_ip($field, array $input, array $params, $value)
     {
         return !$this->validate_valid_ip($field, $input, $params, $value);
     }
@@ -351,7 +351,7 @@ trait InvertValidationTrait
      *
      * @return bool
      */
-    protected function validate_invert_valid_ipv4($field, array $input, array $params = [], $value)
+    protected function validate_invert_valid_ipv4($field, array $input, array $params, $value)
     {
         return !$this->validate_valid_ipv4($field, $input, $params, $value);
     }
@@ -366,7 +366,7 @@ trait InvertValidationTrait
      *
      * @return bool
      */
-    protected function validate_invert_valid_ipv6($field, array $input, array $params = [], $value)
+    protected function validate_invert_valid_ipv6($field, array $input, array $params, $value)
     {
         return !$this->validate_valid_ipv6($field, $input, $params, $value);
     }
@@ -381,7 +381,7 @@ trait InvertValidationTrait
      *
      * @return bool
      */
-    protected function validate_invert_valid_cc($field, array $input, array $params = [], $value)
+    protected function validate_invert_valid_cc($field, array $input, array $params, $value)
     {
         return !$this->validate_valid_cc($field, $input, $params, $value);
     }
@@ -396,7 +396,7 @@ trait InvertValidationTrait
      *
      * @return bool
      */
-    protected function validate_invert_valid_name($field, array $input, array $params = [], $value)
+    protected function validate_invert_valid_name($field, array $input, array $params, $value)
     {
         return !$this->validate_valid_name($field, $input, $params, $value);
     }
@@ -411,7 +411,7 @@ trait InvertValidationTrait
      *
      * @return bool
      */
-    protected function validate_invert_street_address($field, array $input, array $params = [], $value)
+    protected function validate_invert_street_address($field, array $input, array $params, $value)
     {
         return !$this->validate_street_address($field, $input, $params, $value);
     }
@@ -426,7 +426,7 @@ trait InvertValidationTrait
      *
      * @return bool
      */
-    protected function validate_invert_iban($field, array $input, array $params = [], $value)
+    protected function validate_invert_iban($field, array $input, array $params, $value)
     {
         return !$this->validate_iban($field, $input, $params, $value);
     }
@@ -441,7 +441,7 @@ trait InvertValidationTrait
      *
      * @return bool
      */
-    protected function validate_invert_date($field, array $input, array $params = [], $value)
+    protected function validate_invert_date($field, array $input, array $params, $value)
     {
         return !$this->validate_date($field, $input, $params, $value);
     }
@@ -456,7 +456,7 @@ trait InvertValidationTrait
      *
      * @return bool
      */
-    protected function validate_invert_min_age($field, array $input, array $params = [], $value)
+    protected function validate_invert_min_age($field, array $input, array $params, $value)
     {
         return !$this->validate_min_age($field, $input, $params, $value);
     }
@@ -471,7 +471,7 @@ trait InvertValidationTrait
      *
      * @return bool
      */
-    protected function validate_invert_max_numeric($field, array $input, array $params = [], $value)
+    protected function validate_invert_max_numeric($field, array $input, array $params, $value)
     {
         return !$this->validate_max_numeric($field, $input, $params, $value);
     }
@@ -486,7 +486,7 @@ trait InvertValidationTrait
      *
      * @return bool
      */
-    protected function validate_invert_min_numeric($field, array $input, array $params = [], $value)
+    protected function validate_invert_min_numeric($field, array $input, array $params, $value)
     {
         return !$this->validate_min_numeric($field, $input, $params, $value);
     }
@@ -501,7 +501,7 @@ trait InvertValidationTrait
      *
      * @return bool
      */
-    protected function validate_invert_starts($field, array $input, array $params = [], $value)
+    protected function validate_invert_starts($field, array $input, array $params, $value)
     {
         return !$this->validate_starts($field, $input, $params, $value);
     }
@@ -516,7 +516,7 @@ trait InvertValidationTrait
      *
      * @return bool
      */
-    protected function validate_invert_required_file($field, array $input, array $params = [], $value)
+    protected function validate_invert_required_file($field, array $input, array $params, $value)
     {
         return !$this->validate_required_file($field, $input, $params, $value);
     }
@@ -531,7 +531,7 @@ trait InvertValidationTrait
      *
      * @return bool
      */
-    protected function validate_invert_extension($field, array $input, array $params = [], $value)
+    protected function validate_invert_extension($field, array $input, array $params, $value)
     {
         return !$this->validate_extension($field, $input, $params, $value);
     }
@@ -546,7 +546,7 @@ trait InvertValidationTrait
      *
      * @return bool
      */
-    protected function validate_invert_equalsfield($field, array $input, array $params = [], $value)
+    protected function validate_invert_equalsfield($field, array $input, array $params, $value)
     {
         return !$this->validate_equalsfield($field, $input, $params, $value);
     }
@@ -561,7 +561,7 @@ trait InvertValidationTrait
      *
      * @return bool
      */
-    protected function validate_invert_guidv4($field, array $input, array $params = [], $value)
+    protected function validate_invert_guidv4($field, array $input, array $params, $value)
     {
         return !$this->validate_guidv4($field, $input, $params, $value);
     }
@@ -576,7 +576,7 @@ trait InvertValidationTrait
      *
      * @return bool
      */
-    protected function validate_invert_phone_number($field, array $input, array $params = [], $value)
+    protected function validate_invert_phone_number($field, array $input, array $params, $value)
     {
         return !$this->validate_phone_number($field, $input, $params, $value);
     }
@@ -591,7 +591,7 @@ trait InvertValidationTrait
      *
      * @return bool
      */
-    protected function validate_invert_regex($field, array $input, array $params = [], $value)
+    protected function validate_invert_regex($field, array $input, array $params, $value)
     {
         return !$this->validate_regex($field, $input, $params, $value);
     }
@@ -606,7 +606,7 @@ trait InvertValidationTrait
      *
      * @return bool
      */
-    protected function validate_invert_valid_json_string($field, array $input, array $params = [], $value)
+    protected function validate_invert_valid_json_string($field, array $input, array $params, $value)
     {
         return !$this->validate_valid_json_string($field, $input, $params, $value);
     }
@@ -621,7 +621,7 @@ trait InvertValidationTrait
      *
      * @return bool
      */
-    protected function validate_invert_valid_array_size_greater($field, array $input, array $params = [], $value)
+    protected function validate_invert_valid_array_size_greater($field, array $input, array $params, $value)
     {
         return !$this->validate_valid_array_size_greater($field, $input, $params, $value);
     }
@@ -636,7 +636,7 @@ trait InvertValidationTrait
      *
      * @return bool
      */
-    protected function validate_invert_valid_array_size_lesser($field, array $input, array $params = [], $value)
+    protected function validate_invert_valid_array_size_lesser($field, array $input, array $params, $value)
     {
         return !$this->validate_valid_array_size_lesser($field, $input, $params, $value);
     }
@@ -651,7 +651,7 @@ trait InvertValidationTrait
      *
      * @return bool
      */
-    protected function validate_invert_valid_array_size_equal($field, array $input, array $params = [], $value)
+    protected function validate_invert_valid_array_size_equal($field, array $input, array $params, $value)
     {
         return !$this->validate_valid_array_size_equal($field, $input, $params, $value);
     }
@@ -666,7 +666,7 @@ trait InvertValidationTrait
      *
      * @return bool
      */
-    protected function validate_invert_valid_twitter($field, array $input, array $params = [], $value)
+    protected function validate_invert_valid_twitter($field, array $input, array $params, $value)
     {
         return !$this->validate_valid_twitter($field, $input, $params, $value);
     }
