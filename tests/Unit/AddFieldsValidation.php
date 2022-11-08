585fc557a8e8321dbf5527fa2b3c2ce3e629c2d2
Angger Pradana <sonypradana@gmail.com>
2022-11-09 05:58:01 +0700

    feat: single entry validator fileds
    - support adding filed using IteratorAggregate interface

--- tests/Unit/AddFieldsValidation.php
+++ tests/Unit/AddFieldsValidation.php
@@ -41,3 +41,26 @@ it('can add field using method fields', function () {
         ->toEqual($fields)
     ;
 });
+
+it('can add field using iterator array', function () {
+    $fields = new class() implements \IteratorAggregate {
+        private $fields = [
+            'field_1' => 'test',
+            'field_2' => 'test',
+        ];
+
+        public function getIterator(): Traversable
+        {
+            return new \ArrayIterator($this->fields);
+        }
+    };
+
+    $valid = new Validator();
+    $valid->fields($fields);
+    expect($valid->get_fields())
+        ->toEqual([
+            'field_1' => 'test',
+            'field_2' => 'test',
+        ])
+    ;
+});
